7ec914e2cee17a63d220bff0d1a4e6d4a820e761
Junpeng Qiu <qjpchmail@gmail.com>
2016-05-13 03:58:33 -0400

    Fix typos

--- fcitx.el
+++ fcitx.el
@@ -120,7 +120,7 @@
 ;;   For Spacemacs users:
 ;;   ,----
 ;;   | (fcitx-aggressive-setup)
-;;   | (setq fcitx-active-evil-states '(insert emacs hybird)) ; if you use hybrid mode
+;;   | (setq fcitx-active-evil-states '(insert emacs hybrid)) ; if you use hybrid mode
 ;;   | (fcitx-prefix-keys-add "M-m") ; M-m is common in Spacemacs
 ;;   | ;; (setq fcitx-use-dbus t) ; uncomment if you're using Linux
 ;;   `----
