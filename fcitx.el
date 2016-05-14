194f8aaa8231dacf7b6c54a7c3cc5cacc43cc826
Junpeng Qiu <qjpchmail@gmail.com>
2016-05-13 22:05:26 -0400

    Correct Spacemacs settings

--- fcitx.el
+++ fcitx.el
@@ -21,8 +21,6 @@
 
 ;;; Commentary:
 
-;; Chinese version of README: https://github.com/cute-jumper/fcitx.el/blob/master/README-zh.org
-
 ;;                              _____________
 
 ;;                                 FCITX.EL
@@ -119,8 +117,9 @@
 
 ;;   For Spacemacs users:
 ;;   ,----
-;;   | (fcitx-aggressive-setup)
+;;   | ;; Make sure the following comes before `(fcitx-aggressive-setup)'
 ;;   | (setq fcitx-active-evil-states '(insert emacs hybrid)) ; if you use hybrid mode
+;;   | (fcitx-aggressive-setup)
 ;;   | (fcitx-prefix-keys-add "M-m") ; M-m is common in Spacemacs
 ;;   | ;; (setq fcitx-use-dbus t) ; uncomment if you're using Linux
 ;;   `----
